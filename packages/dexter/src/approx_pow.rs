16f11d50d156ccaa977e2b77e550e15a7fe57c49
CyberHoward <CyberHoward@protonmail.com>
2022-08-02 15:37:40 +0000

    fix test and add return Result

--- packages/dexter/src/approx_pow.rs
+++ packages/dexter/src/approx_pow.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-use cosmwasm_std::{Decimal, Uint128};
+use cosmwasm_std::{Decimal, Uint128, StdResult, StdError};
 
 fn abs_difference_with_sign(a: Decimal, b: Decimal) -> (Decimal, bool) {
 	if a >= b {
@@ -10,17 +10,17 @@ fn abs_difference_with_sign(a: Decimal, b: Decimal) -> (Decimal, bool) {
 	}
 }
 
-fn pow_approx(base: Decimal, exp: Decimal, precision: Option<Decimal>)-> Decimal {
+pub fn pow_approx(base: Decimal, exp: Decimal, precision: Option<Decimal>)-> StdResult<Decimal> {
 	let precision = precision.unwrap_or_else(|| Decimal::from_str("0.00000001").unwrap());
-	if exp.is_zero() {return base};
+	if exp.is_zero() {return Ok(base)};
 	let base = base.clone();
 	let (x, xneg) = abs_difference_with_sign(base, Decimal::one());
 	let mut term = Decimal::one();
 	let mut sum = Decimal::one();
 	let mut negative = false;
 
-	let mut a = exp.clone();
-	let mut bigK = Decimal::zero();
+	let a = exp.clone();
+	let mut big_k = Decimal::zero();
 	// TODO: Document this computation via taylor expansion
 	let mut i = 1u128;
 
@@ -28,13 +28,13 @@ fn pow_approx(base: Decimal, exp: Decimal, precision: Option<Decimal>)-> Decimal
 		// At each iteration, we need two values, i and i-1.
 		// To avoid expensive big.Int allocation, we reuse bigK variable.
 		// On this line, bigK == i-1.
-		let (c, cneg) = abs_difference_with_sign(a, bigK);
+		let (c, cneg) = abs_difference_with_sign(a, big_k);
 
 		// On this line, bigK == i.
-		bigK = Decimal::from_atomics(Uint128::from(i), 0).unwrap();
+		big_k = Decimal::from_atomics(Uint128::from(i), 0).map_err(|e|StdError::generic_err(e.to_string()))?;
 
-        println!("term: {}, c: {}, x: {}, k: {}",&term, &c, &x, &bigK);
-		term = (term * c * x) / bigK;
+        // println!("term: {}, c: {}, x: {}, k: {}",&term, &c, &x, &big_k);
+		term = (term * c * x) / big_k;
 
 		// a is mutated on absDifferenceWithSign, reset
 		
@@ -56,7 +56,7 @@ fn pow_approx(base: Decimal, exp: Decimal, precision: Option<Decimal>)-> Decimal
 		}
         i+=1;
 	}
-	return sum
+	return Ok(sum)
 }
 
 #[cfg(test)]
@@ -67,7 +67,6 @@ use super::*;
 		let res = pow_approx(Decimal::from_str("0.8").unwrap(), 
 		Decimal::from_str("0.32").unwrap(), 
 		Some(Decimal::from_str("0.00000001").unwrap()));
-		assert_eq!(res, Decimal::from_str("0.93108385").unwrap())
+		assert_eq!(&res.unwrap().to_string()[0..10], "0.93108385")
 	}
 }
-
