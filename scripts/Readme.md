38c391ac7c9b35eeb0d5941698622a36575ec739
Alien hunter no. 42 <84127070+alienHunterOnMars@users.noreply.github.com>
2022-08-15 14:51:50 +0400

    updated readme

---
+++ scripts/Readme.md
@@ -0,0 +1,8 @@
+## Dexter - Deployment Guide
+
+- This repo contains some helper scripts for deploying Dexter contracts on the Persistence Network.
+
+```
+npm install
+node --loader ts-node/esm testnet_deploy.ts
+```
--- scripts/testnet_deploy.ts
+++ scripts/testnet_deploy.ts
@@ -1,17 +1,14 @@
-import { PersistenceClient } from "persistenceonejs";
+// import { PersistenceClient } from "persistenceonejs";
 import { coins } from "@cosmjs/stargate";
 
 async function Demo() {
-  const alice = await PersistenceClient.init(
-    "obtain door word season wealth inspire tobacco shallow thumb tip walk forum someone verb pistol bright mutual nest fog valley tiny section sauce typical"
-  ); //persistence1ht0tun4u5uj4f4z83p9tncjerwu27ycsm52txm
-
-  const codes = await alice.query.cosmwasm.wasm.v1.codes({});
-  console.log(codes);
-
-  const [account] = await alice.wallet.getAccounts();
-  const aliceaddress = account.address; //persistence1ht0tun4u5uj4f4z83p9tncjerwu27ycsm52txm
-
+  // const alice = await PersistenceClient.init(
+  //   "obtain door word season wealth inspire tobacco shallow thumb tip walk forum someone verb pistol bright mutual nest fog valley tiny section sauce typical"
+  // ); //persistence1ht0tun4u5uj4f4z83p9tncjerwu27ycsm52txm
+  // const codes = await alice.query.cosmwasm.wasm.v1.codes({});
+  // console.log(codes);
+  // const [account] = await alice.wallet.getAccounts();
+  // const aliceaddress = account.address; //persistence1ht0tun4u5uj4f4z83p9tncjerwu27ycsm52txm
   // const pstake =
   //   "persistence14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sjvz4fk"; //cw20 token address on chain
   // const res = await alice.wasm.execute(
@@ -26,7 +23,6 @@ async function Demo() {
   //   { amount: coins(2_000_000, "stake"), gas: "200000" }
   // );
   // console.log(res);
-
   // const balance = await alice.wasm.queryContractSmart(pstake, {
   //   balance: { address: "persistence123em6jp7y96rtylp6tjk9r0dcescl0k4ccqvpu" },
   // });
