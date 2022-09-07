ba9cd0d80c331a674a8d5387b88bd6d18881a806
Alien hunter no. 42 <alienHunterOnMars@protonmail.com>
2022-09-07 10:39:07 +0400

    update scripts

--- scripts/helpers/helpers.ts
+++ scripts/helpers/helpers.ts
@@ -1,10 +1,10 @@
-import { PersistenceClient, cosmos, cosmwasm } from "persistenceonejs";
+import { PersistenceClient, cosmos, cosmwasm } from "cosmoschainsjs";
 import { coins, Coin } from "@cosmjs/stargate";
 import { Any } from "cosmjs-types/google/protobuf/any.js";
-// import { Any } from "cosmjs-types/google/protobuf/any";
-import * as Long from "long";
+import Long  from "long";
 import { readFileSync, writeFileSync } from "fs";
 import path from "path";
+
 
 export const ARTIFACTS_PATH = "../artifacts";
 
@@ -28,40 +28,270 @@ export function writeArtifact(data: object, name: string = "artifact") {
   );
 }
 
-// --------x-------------x-------------x----- -----x---------
-// --------x----- QUERY :: Helpers Functions-----x-----------
-// --------x-------------x-------------x----- -----x---------
+
+// --------x-------------x-------------x----- -----x-----------------x-----------
+// --------x----- WASM MODULE :::: QUERIES :: Helpers Functions -----x-----------
+// --------x-------------x-------------x----- -----x-----------------x-----------
+
+
+// ContractInfo gets the contract meta data
+export async function query_wasm_contractInfo(
+  client: PersistenceClient,
+  contract_addr: string
+) {
+  let response = await client.query.cosmwasm.wasm.v1.contractInfo(
+    cosmwasm.wasm.v1.QueryContractInfoRequest.fromPartial({ address: contract_addr })
+  );
+  return response;
+}
+
+// ContractHistory gets the contract code history
+export async function query_wasm_contractHistory(
+  client: PersistenceClient,
+  contract_addr: string,
+  offset?: number,
+  limit?: number
+) {
+  let response = await client.query.cosmwasm.wasm.v1.contractHistory(
+    cosmwasm.wasm.v1.QueryContractHistoryRequest.fromPartial({ address: contract_addr,
+    pagination: cosmos.base.query.v1beta1.PageRequest.fromPartial({offset: offset, limit: limit}) })
+  );
+  return response;
+}
+
+// ContractsByCode lists all smart contracts for a code id
+export async function query_wasm_contractsByCode(
+  client: PersistenceClient,
+  codeId: number,
+  offset?: number,
+  limit?: number,
+) {
+  let codes = await client.query.cosmwasm.wasm.v1.contractsByCode(
+    cosmwasm.wasm.v1.QueryContractsByCodeRequest.fromPartial({ codeId: codeId ,
+    pagination: cosmos.base.query.v1beta1.PageRequest.fromPartial({offset: offset, limit: limit})} )
+  );
+  return codes;
+}
+
+// AllContractState gets all raw store data for a single contract
+export async function query_wasm_allContractState(
+  client: PersistenceClient,
+  address: string,
+  offset?: number,
+  limit?: number,
+) {
+  let response = await client.query.cosmwasm.wasm.v1.allContractState(
+    cosmwasm.wasm.v1.QueryAllContractStateRequest.fromPartial({ address: address,
+    pagination: cosmos.base.query.v1beta1.PageRequest.fromPartial({offset: offset, limit: limit})} )
+  );
+  return response;
+}
+
+// RawContractState gets single key from the raw store data of a contract
+export async function query_wasm_rawContractState(
+  client: PersistenceClient,
+  address: string,
+  queryData: Uint8Array
+) {
+  let response = await client.query.cosmwasm.wasm.v1.rawContractState(
+    cosmwasm.wasm.v1.QueryRawContractStateRequest.fromPartial({ address: address, queryData: queryData })
+  );
+  return response;
+}
+
+// SmartContractState get smart query result from the contract
+export async function query_wasm_smartContractState(
+  client: PersistenceClient,
+  address: string,
+  queryData: Uint8Array
+) {
+  let response = await client.query.cosmwasm.wasm.v1.smartContractState(
+    cosmwasm.wasm.v1.QuerySmartContractStateRequest.fromPartial({ address: address, queryData: queryData })
+  );
+  return response;
+}
+
+// Code gets the binary code and metadata for a singe wasm code
+export async function query_wasm_code(
+  client: PersistenceClient,
+  code_id: number
+) {
+  let response = await client.query.cosmwasm.wasm.v1.code(
+    cosmwasm.wasm.v1.QueryCodeRequest.fromPartial({ codeId: code_id })
+  );
+  return response;
+}
+
+// Code gets the binary code and metadata for a singe wasm code
+export async function query_wasm_codes(
+  client: PersistenceClient,
+  offset?: number,
+  limit?: number
+) {
+  let response = await client.query.cosmwasm.wasm.v1.codes(
+    cosmwasm.wasm.v1.QueryCodesRequest.fromPartial({ pagination: cosmos.base.query.v1beta1.PageRequest.fromPartial({offset: offset, limit: limit}) })
+  );
+  return response;
+}
+
+// PinnedCodes gets the pinned code ids
+export async function query_wasm_pinnedCodes(
+  client: PersistenceClient,
+  offset?: number,
+  limit?: number
+) {
+  let response = await client.query.cosmwasm.wasm.v1.pinnedCodes(
+    cosmwasm.wasm.v1.QueryPinnedCodesRequest.fromPartial({ pagination: cosmos.base.query.v1beta1.PageRequest.fromPartial({offset: offset, limit: limit}) })
+  );
+  return response;
+}
+
 
 // Return list of codeIds with codeInfos having datahash for the contract
-export async function getCodeIdsListWithPrintedHexHashes(
-  client: PersistenceClient
+export async function find_code_id_from_contract_hash(
+  client: PersistenceClient,
+  hash: string,
+  offset?: number,
+  limit?: number
 ) {
-  let codes = await client.query.cosmwasm.wasm.v1.codes({});
+  let codes = await query_wasm_codes(client, offset, limit);
   let codeInfos = codes["codeInfos"];
-  let pagination = codes["pagination"];
 
   for (let i = 0; i < codeInfos.length; i++) {
     let hex = Buffer.from(codeInfos[i]["dataHash"]).toString("hex");
     let code_id = codeInfos[i]["codeId"];
     console.log(` code_id = ${code_id} hex = ${hex}`);
+    if (hash == hex) {
+      return code_id;
+    }
+
   }
-  return codes;
+  return 0;
 }
 
-// Return address of instantiated contract instance address for a given codeId
-export async function getContractsByCodeId(
+
+// --------x-------------x-------------x----- -----x-----------------x-----------
+// --------x----- GOV MODULE :::: QUERIES :: Helpers Functions -----x-----------
+// --------x-------------x-------------x----- -----x-----------------x-----------
+
+
+// Proposal queries proposal details based on ProposalID
+  // PROPOSAL_STATUS_UNSPECIFIED = 0
+  // PROPOSAL_STATUS_DEPOSIT_PERIOD = 1
+  // PROPOSAL_STATUS_VOTING_PERIOD = 2
+  // PROPOSAL_STATUS_PASSED = 3
+  // PROPOSAL_STATUS_REJECTED = 4
+  // PROPOSAL_STATUS_FAILED = 5
+export async function query_gov_proposal(
   client: PersistenceClient,
-  codeId: number
+  proposalId: number
 ) {
-  let codes = await client.query.cosmwasm.wasm.v1.contractsByCode(
-    cosmwasm.wasm.v1.QueryContractsByCodeRequest.fromPartial({ codeId: 8 })
+  let response = await client.query.cosmos.gov.v1beta1.proposal(
+    cosmos.gov.v1beta1.QueryProposalRequest.fromPartial({proposalId: proposalId}) 
   );
-  return codes;
+  return response;
 }
 
-// --------x-------------x-------------x----- -----x---------
-// --------x----- EXECUTE CONTRACT :: Helpers Functions-----x-----------
-// --------x-------------x-------------x----- -----x---------
+// Proposals queries all proposals based on given status.
+export async function query_gov_proposals(
+  client: PersistenceClient,
+  proposalStatus: number,
+  voter: string,
+  depositor: string,
+  limit?: number,
+  offset?:number 
+) {
+  let response = await client.query.cosmos.gov.v1beta1.proposals(
+    cosmos.gov.v1beta1.QueryProposalsRequest.fromPartial({ 
+      proposalStatus: cosmos.gov.v1beta1.proposalStatusFromJSON(proposalStatus),
+      voter: voter,
+      depositor: depositor,
+      pagination: cosmos.base.query.v1beta1.PageRequest.fromPartial({offset: offset, limit: limit})
+     })
+  );
+  return response;
+}
+
+// Vote queries voted information based on proposalID, voterAddr
+export async function query_gov_vote(
+  client: PersistenceClient,
+  proposalId: number,
+  voter: string
+) {
+  let response = await client.query.cosmos.gov.v1beta1.vote(
+    cosmos.gov.v1beta1.QueryVoteRequest.fromPartial({ proposalId: proposalId, voter: voter })
+  );
+  return response;
+}
+
+// Votes queries votes of a given proposal.
+export async function query_gov_votes(
+  client: PersistenceClient,
+  proposalId: number,
+  offset?: number,
+  limit?: number
+) {
+  let response = await client.query.cosmos.gov.v1beta1.votes(
+    cosmos.gov.v1beta1.QueryVotesRequest.fromPartial({ proposalId: proposalId, 
+      pagination: cosmos.base.query.v1beta1.PageRequest.fromPartial({offset: offset, limit: limit})
+     })
+  );
+  return response;
+}
+
+// Params queries all parameters of the gov module.
+// params_type defines which parameters to query for, can be one of "voting", "tallying" or "deposit".
+export async function query_gov_params(
+  client: PersistenceClient,
+  paramsType: string
+) {
+  let response = await client.query.cosmos.gov.v1beta1.params(
+    cosmos.gov.v1beta1.QueryParamsRequest.fromPartial({ paramsType: paramsType })
+  );
+  return response;
+}
+
+// Deposit queries single deposit information based proposalID, depositAddr.
+export async function query_gov_deposit(
+  client: PersistenceClient,
+  proposalId: number,
+  depositor: string
+) {
+  let response = await client.query.cosmos.gov.v1beta1.deposit(
+    cosmos.gov.v1beta1.QueryDepositRequest.fromPartial({ proposalId: proposalId, depositor: depositor })
+  );
+  return response;
+}
+
+// Deposits queries all deposits of a single proposal.
+export async function query_gov_deposits(
+  client: PersistenceClient,
+  proposalId: number,
+  offset?: number,
+  limit?: number
+) {
+  let response = await client.query.cosmos.gov.v1beta1.deposits(
+    cosmos.gov.v1beta1.QueryDepositsRequest.fromPartial({  proposalId: proposalId, 
+      pagination: cosmos.base.query.v1beta1.PageRequest.fromPartial({offset: offset, limit: limit}) })
+  );
+  return response;
+}
+
+// TallyResult queries the tally of a proposal vote.
+export async function query_gov_tallyresult(
+  client: PersistenceClient,
+  proposalId: number,
+) {
+  let response = await client.query.cosmos.gov.v1beta1.tallyResult(
+    cosmos.gov.v1beta1.QueryTallyResultRequest.fromPartial({ proposalId: proposalId })
+  );
+  return response;
+}
+
+
+// --------x-------------x-------------x----- -----x--------------x-----------
+// --------x----- EXECUTE CONTRACT :: Helpers Functions      -----x-----------
+// --------x-------------x-------------x----- -----x--------------x-----------
 
 export async function executeContract(
   client: PersistenceClient,
@@ -88,60 +318,28 @@ export async function executeContract(
   }
 }
 
+// --------x-------------x-------------x----- -----x---------
 // --------x----- Governance Module Helpers -----x-----------
 // --------x-------------x-------------x----- -----x---------
 
-/// GOV MODULE -- SUBMIT PROPOSAL ExecuteMsg
+/// GOV MODULE -- SUBMIT PROPOSAL ExecuteMssg
 /// Network : 512 XPRT are required as deposit to deploy the contract
 /// Tokens transferred - initialDeposit amount is transferred from user a/c to the module address
 export async function Gov_MsgSubmitProposal(
   client: PersistenceClient,
-  proposal: { typeUrl?: string; value?: Uint8Array }
+  proposal: { typeUrl?: string; value?: Uint8Array },
+  denom: string,
+  deposit: number
 ) {
+  let signer = client.config;
   const [account] = await client.wallet.getAccounts();
   //submit proposal Msg
   const proposalMsg = {
     typeUrl: "/cosmos.gov.v1beta1.MsgSubmitProposal",
     value: {
       content: Any.fromPartial(proposal),
-      initialDeposit: coins(512_000_000, "uxprt"),
+      initialDeposit: coins(deposit, denom),
       proposer: account.address,
-    },
-  };
-  // sign & broadcast the transaction
-  const res = await client.core.signAndBroadcast(
-    account.address,
-    [proposalMsg],
-    { amount: coins(2_000_000, "uxprt"), gas: "20000000" },
-    "Proposal Submitted!"
-  );
-  // Handle the response
-  if (res.code === 0) {
-    const json = JSON.parse(res.rawLog);
-    console.log("Transactions executed successfully => ", res.transactionHash);
-    let proposalId = json[0].events[3].attributes[1].value;
-    console.log(`Proposal Id is = ${proposalId} `);
-    return json; //return json response, for proposalID use json[0].events[3].attributes[1].value
-  } else {
-    return res.rawLog;
-  }
-}
-
-/// GOV MODULE -- Deposit with Propsoal - ExecuteMsg
-export async function Gov_MsgDeposit(
-  client: PersistenceClient,
-  proposal_id: number,
-  amount: number,
-  denom: string
-) {
-  const [account] = await client.wallet.getAccounts();
-  // Deposit with Propsoal
-  const proposalMsg = {
-    typeUrl: "/cosmos.gov.v1beta1.MsgDeposit",
-    value: {
-      proposal_id: Long.fromNumber(proposal_id),
-      depositor: account.address,
-      amount: coins(amount, denom),
     },
   };
   // sign & broadcast the transaction
@@ -156,17 +354,54 @@ export async function Gov_MsgDeposit(
     const json = JSON.parse(res.rawLog);
     console.log("Transactions executed successfully => ", res.transactionHash);
     let proposalId = json[0].events[3].attributes[1].value;
+    console.log(`Proposal Id is = ${proposalId} `);
+    return json; //return json response, for proposalID use json[0].events[3].attributes[1].value
+  } else {
+    return res.rawLog;
+  }
+}
+
+/// GOV MODULE -- Deposit with Propsoal - ExecuteMssg
+export async function Gov_MsgDeposit(
+  client: PersistenceClient,
+  proposal_id: number,
+  amount: number,
+  denom: string
+) {
+  const [account] = await client.wallet.getAccounts();
+  // Deposit with Propsoal
+  const depositMsg = {
+    typeUrl: "/cosmos.gov.v1beta1.MsgDeposit",
+    value: {
+      proposal_id: Long.fromNumber(proposal_id),
+      depositor: account.address,
+      amount: coins(amount, denom),
+    },
+  };
+  // sign & broadcast the transaction
+  const res = await client.core.signAndBroadcast(
+    account.address,
+    [depositMsg],
+    { amount: coins(2_000_000, denom), gas: "20000000" },
+    "Proposal Submitted!"
+  );
+  // Handle the response
+  if (res.code === 0) {
+    const json = JSON.parse(res.rawLog);
+    console.log("Transactions executed successfully => ", res.transactionHash);
+    let proposalId = json[0].events[3].attributes[1].value;
     return json;
   } else {
     return res.rawLog;
   }
 }
 
-/// GOV MODULE -- VOTE on Propsoal - ExecuteMsg
+/// GOV MODULE -- VOTE on Propsoal - ExecuteMssg
 export async function voteOnProposal(
   client: PersistenceClient,
   proposalid: number,
-  vote: number
+  vote: number,
+  denom: string
 ) {
   const [account] = await client.wallet.getAccounts();
   // Vote on Propsoal
@@ -182,8 +417,7 @@ export async function voteOnProposal(
   const res = await client.core.signAndBroadcast(
     account.address,
     [sendMsg],
-    { amount: coins(10_000_000, "stake"), gas: "2000000" },
-    "Vote Yes!"
+    { amount: coins(10_000_000, denom), gas: "2000000" }
   );
   if (res.code === 0) {
     return res;
@@ -263,3 +497,12 @@ export async function MultiSend(
   );
   console.log(res);
 }
+
+
+
+
+
+
+
+
+
