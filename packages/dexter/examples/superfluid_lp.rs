6eafba81326a7393d5a15e314b6f421eb94eb9f3
Max <maksim@persistence.one>
2023-11-30 04:39:19 +0000

    chore: generate schema and examples for superfluid_lp

---
+++ packages/dexter/examples/superfluid_lp.rs
@@ -0,0 +1,10 @@
+use cosmwasm_schema::write_api;
+use dexter::superfluid_lp::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        query: QueryMsg,
+        execute: ExecuteMsg,
+    }
+}
